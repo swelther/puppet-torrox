f99d95fbf94374372d2c259bcabe66c2c2351ed1
Sebastian Welther <swelther@siewert-kau.de>
2016-10-13 11:14:53 +0200

    Allow to disable passenger version in header
    Works only with Passenger 5.0.x

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -2,23 +2,25 @@
 #
 #
 define rails::passenger(
-  $ruby_version       = $title,
-  $passenger_version  = $rails::params::passenger_version,
-  $mininstances       = '1',
-  $maxinstancesperapp = '5',
-  $maxpoolsize        = '10',
-  $poolidletime       = '300',
+  $ruby_version              = $title,
+  $passenger_version         = $rails::params::passenger_version,
+  $mininstances              = '1',
+  $maxinstancesperapp        = '5',
+  $maxpoolsize               = '10',
+  $poolidletime              = '300',
+  $disable_version_in_header = false,
 ) {
   if "$::rvm_installed" == 'true' {
     class { 'rvm::passenger::apache':
-      version            => $passenger_version,
-      ruby_version       => $ruby_version,
-      require            => [ Rvm_system_ruby[$ruby_version], Service['apache2'] ],
-      mininstances       => $mininstances,
-      maxinstancesperapp => $maxinstancesperapp,
-      maxpoolsize        => $maxpoolsize,
-      poolidletime       => $poolidletime,
-      spawnmethod        => 'smart-lv2';
+      version                   => $passenger_version,
+      ruby_version              => $ruby_version,
+      require                   => [ Rvm_system_ruby[$ruby_version], Service['apache2'] ],
+      mininstances              => $mininstances,
+      maxinstancesperapp        => $maxinstancesperapp,
+      maxpoolsize               => $maxpoolsize,
+      poolidletime              => $poolidletime,
+      spawnmethod               => 'smart-lv2',
+      disable_version_in_header => $disable_version_in_header,
     }
   }
 
